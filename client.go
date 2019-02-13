493e86d88675ee0f02f40f37e71170aa567b312d
sudhakarshah <sudhakarshah2013@gmail.com>
2019-02-12 22:55:22 -0600

    name added

--- client.go
+++ client.go
@@ -16,19 +16,25 @@ const (
 		CONN_TYPE = "tcp"
 )
 
-// fixed size array containing all the IP addresses
-var IpAddress = [...]string {"172.22.94.77", "172.22.156.69"}
-// var IpAddress = [...]string {"localhost"}
+type message struct {
+	name string
+	data string
+}
 
+
+// fixed size array containing all the IP addresses
+var IpAddress = [...]string {"172.22.94.77", "172.22.156.69", "172.22.158.69"}
+// var IpAddress = [...]string {"localhost"}
+var name string
 var wg sync.WaitGroup
 func main() {
 		// argsWithProg := os.Args
 		arguments := os.Args[1:]
-		name := arguments[0]
+		name = arguments[0]
 		port := arguments[1]
 		fmt.Println(name+ port)
 		numberOfParticipants, err := strconv.Atoi(arguments[2])
-		if (err!=nil) {
+		if (err != nil) {
 
 		}
 
@@ -83,14 +89,16 @@ func handleRequest(conn net.Conn) {
 	fmt.Println("New connection added to server")
 	buf := make([]byte, 1024)
 	// Read the incoming connection into the buffer.
-	// reqLen, err := conn.Read(buf)
-	reqLen, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+	for {
+		recLen, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+		}
+		fmt.Printf("received %v\n", string(buf[:int(recLen)]))
+		// Send a response back to person contacting us.
+		// conn.Write([]byte("Message received."))
 	}
-	fmt.Println(reqLen)
-	// Send a response back to person contacting us.
-	conn.Write([]byte("Message received."))
+
 	// Close the connection when you're done with it.
 	conn.Close()
 }
@@ -103,7 +111,7 @@ func client(address string) {
 
 	// loop till client can't connect to server
 	for err != nil {
-		fmt.Println("retryig connecting to server")
+		// fmt.Println("retryig connecting to server")
 		conn, err = net.Dial("tcp", address)
 		// can introduce some sleep here
 	}
@@ -113,8 +121,9 @@ func client(address string) {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("Text to send to "+ address)
 		text, _ := reader.ReadString('\n')
+		msg := message{name, text}
 		// send to socket
-		fmt.Fprintf(conn, text + "\n")
+		fmt.Fprintf(conn, msg.name +": " + msg.data + "\n")
 		// listen for reply
 		// message, _ := bufio.NewReader(conn).ReadString('\n')
 		// fmt.Print("Message from server: "+message)
